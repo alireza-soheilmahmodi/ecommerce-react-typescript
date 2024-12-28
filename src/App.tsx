af380497d34c4e928e837abecf2b85c76a15e1fb
alireza <alirezasoheilmahmodi@gmail.com>
2024-12-28 18:03:34 +0330

    frontend init is completed and shadcn with tailwind are ready

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-background">hello there</h1>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
